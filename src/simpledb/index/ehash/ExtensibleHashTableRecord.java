ecbc42d2b4c5b5952eb836c516bbc37ef72fbc1f
skyler <konata.izumi20@gmail.com>
2018-04-23 12:10:44 -0400

    Added this to make storing hash table records easier

---
+++ src/simpledb/index/ehash/ExtensibleHashTableRecord.java
@@ -0,0 +1,35 @@
+package simpledb.index.ehash;
+
+public class ExtensibleHashTableRecord {
+
+    private String recHash;
+    private int recFreeSpots;
+    private int recLocalDepth;
+
+    public ExtensibleHashTableRecord(String recHash, int recFreeSpots, int recLocalDepth, String recPointerTable) {
+        this.recHash = recHash;
+        this.recFreeSpots = recFreeSpots;
+        this.recLocalDepth = recLocalDepth;
+        this.recPointerTable = recPointerTable;
+    }
+
+    public String getRecHash() {
+
+        return recHash;
+    }
+
+    public int getRecFreeSpots() {
+        return recFreeSpots;
+    }
+
+    public int getRecLocalDepth() {
+        return recLocalDepth;
+    }
+
+    public String getRecPointerTable() {
+        return recPointerTable;
+    }
+
+    private String recPointerTable;
+
+}
